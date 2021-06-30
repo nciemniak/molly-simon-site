1b5ce7c161eec6c42bd01eceec2b4e46f03a5ea5
nciemniak <nciemniak@gmail.com>
2021-06-30 13:53:26 -0500

    click outside of mobile nav menu to close it

--- static/js/index.js
+++ static/js/index.js
@@ -5,10 +5,25 @@ $(function() {
     $('#mobile-nav-menu').slideDown();
   });
 
-  // close mobile nav menu
+  // click "x" to close mobile nav menu
   $('img#close-icon').on('click', function() {
     $(".dim-overlay").fadeOut();
     $('#mobile-nav-menu').slideUp();
+  });
+
+  // clicking anywhere on the page will also trigger close
+  $(window).on('click', function(event) {
+    if (event.target.id == 'mobile-nav-menu-icon')
+      return;
+    $(".dim-overlay").fadeOut();
+    $('#mobile-nav-menu').slideUp();
+  });
+  
+  //stop menu from closing if clicking within menu
+  $('#mobile-nav-menu').on('click', function(event) {
+    if(event.target.id == "close-icon")
+      return;
+    event.stopPropagation();
   });
 });
 
@@ -18,7 +33,6 @@ function goToPastProjects(url) {
   window.location.href = url + query_params;
 }
 
-// research dropdown navigation
 function goToResearchGroup(url) {
   query_params = "?section=reserach_group"
   window.location.href = url + query_params;
